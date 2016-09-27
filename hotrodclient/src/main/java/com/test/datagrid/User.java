0868e6c5424f8f76ce849a9b5c088bd211289e6c
jonkey <zeguan@redhat.com>
2016-09-27 17:47:08 +0800

    first commit

---
+++ hotrodclient/src/main/java/com/test/datagrid/User.java
@@ -0,0 +1,42 @@
+package com.test.datagrid;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -1314959919648108817L;
+
+    public User() {
+    };
+
+    public User(String id, String data) {
+	super();
+	this.id = id;
+	this.data = data;
+    }
+
+    private String id;
+    private String data;
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getDate() {
+	return data;
+    }
+
+    public void setData(String data) {
+	this.data = data;
+    }
+
+    @Override
+    public String toString() {
+	return "User [id=" + id + ", data=" + data + "]";
+    }
+
+}
---
+++ hotrodclient1/src/main/java/com/test/datagrid/DistributedCache.java
@@ -0,0 +1,23 @@
+package com.test.datagrid;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+
+public class DistributedCache {
+
+    private static RemoteCacheManager cacheManager;
+    private static RemoteCache<String, User> cache;
+
+    static {
+	cacheManager = new RemoteCacheManager();
+	cache = cacheManager.getCache("myCache");
+    }
+
+    public static void put(String id, User user) {
+	cache.put(id, user);
+    }
+
+    public static User get(String id) {
+	return cache.get(id);
+    }
+}
