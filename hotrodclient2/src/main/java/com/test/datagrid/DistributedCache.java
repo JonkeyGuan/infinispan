aea7d633beeb02d45a334d419be51cfadf008c21
Zefa Guan <zeguan@redhat.com>
2016-12-18 16:45:52 +0800

    changed hotrod client 2 to 6.4.1 replication

--- hotrodclient2/src/main/java/com/test/datagrid/DistributedCache.java
+++ hotrodclient2/src/main/java/com/test/datagrid/DistributedCache.java
@@ -5,19 +5,19 @@ import org.infinispan.client.hotrod.RemoteCacheManager;
 
 public class DistributedCache {
 
-    private static RemoteCacheManager cacheManager;
-    private static RemoteCache<String, User> cache;
+	private static RemoteCacheManager cacheManager;
+	private static RemoteCache<String, User> cache;
 
-    static {
-	cacheManager = new RemoteCacheManager();
-	cache = cacheManager.getCache("myCache");
-    }
+	static {
+		cacheManager = new RemoteCacheManager();
+		cache = cacheManager.getCache("myCache");
+	}
 
-    public static void put(String id, User user) {
-	cache.put(id, user);
-    }
+	public static void put(String id, User user) {
+		cache.put(id, user);
+	}
 
-    public static User get(String id) {
-	return cache.get(id);
-    }
+	public static User get(String id) {
+		return cache.get(id);
+	}
 }
--- hotrodclient2/src/main/java/com/test/datagrid/Run.java
+++ hotrodclient2/src/main/java/com/test/datagrid/Run.java
@@ -1,59 +1,77 @@
 package com.test.datagrid;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Run {
 
-    private static AtomicLong seq = new AtomicLong(0);
+	private final static Logger log = LoggerFactory.getLogger(Run.class);
 
-    private static String data = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234";
+	private static AtomicLong seq = new AtomicLong(0);
 
-    public static void main(String[] args) {
-	String type = "put";
-	if (args != null && args.length > 0) {
-	    if (!args[0].equals(type)) {
-		type = args[0];
-	    }
+	private static String data = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234";
+
+	public static void main(String[] args) {
+		String type = "put";
+		if (args != null && args.length > 0) {
+			if (!args[0].equals(type)) {
+				type = args[0];
+			}
+		}
+
+		if (type.equals("put")) {
+			put();
+		} else {
+			get();
+		}
 	}
 
-	if (type.equals("put")) {
-	    put();
-	} else {
-	    get();
-	}
-    }
+	public static void put() {
+		// int i = 0;
+		while (true) {
 
-    public static void put() {
-	int i = 0;
-	while (true) {
-	    String id = "user" + seq.getAndIncrement();
-	    User user = new User(id, data);
-	    DistributedCache.put(id, user);
-	    System.out.println("put: " + user);
-//	    try {
-//		Thread.sleep(1);
-//	    } catch (InterruptedException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	    }
-	    if (i > 1024 * 1024 * 6) {
-		break;
-	    }
-	}
-    }
+			if (seq.get() > 150000)
+				break;
 
-    public static void get() {
-	while (true) {
-	    long i = ThreadLocalRandom.current().nextLong(1000000);
-	    String id = "user" + i;
-	    User user = DistributedCache.get(id);
-	    if (user == null) {
-		System.out.println("can't get user by " + id);
-	    } else {
-		System.out.println(user);
-	    }
+			try {
+				String id = "user" + seq.incrementAndGet();
+				User user = new User(id, data);
+				DistributedCache.put(id, user);
+				String dateText = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+				log.info(dateText + "	put	" + id);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-    }
+
+	public static void get() {
+		String id = "user" + seq.incrementAndGet();
+		while (true) {
+			try {
+				// long i = ThreadLocalRandom.current().nextLong(1000000);
+
+				if (seq.get() > 150000)
+					break;
+
+				User user = DistributedCache.get(id);
+				if (user == null) {
+					// system.out.println("can't get user by " + id);
+				} else {
+					String dateText = LocalDateTime.now()
+							.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+					log.info(dateText + "	get	" + id);
+					id = "user" + seq.incrementAndGet();
+				}
+				// Thread.sleep(3000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
