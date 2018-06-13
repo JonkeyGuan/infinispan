101f06fad08f3279461d92e0bb51cfbb74ceb994
Jonkey Guan <jonkey.guan@gmail.com>
2018-06-13 15:19:49 +0800

    added stress test

---
+++ stresstest/src/main/java/com/test/datagrid/Run.java
@@ -0,0 +1,22 @@
+package com.test.datagrid;
+
+public class Run {
+
+	public static void main(String[] args) {
+		int threadCount = 10;
+		if (args != null && args.length > 0) {
+			String threadCountText = args[0];
+			try {
+				threadCount = Integer.parseInt(threadCountText);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for (int i = 0; i < threadCount; i++) {
+			new Tester().run();
+		}
+
+	}
+
+}
---
+++ stresstest/src/main/java/com/test/datagrid/Tester.java
@@ -0,0 +1,52 @@
+package com.test.datagrid;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tester implements Runnable {
+
+	private final static Logger log = LoggerFactory.getLogger(Tester.class);
+
+	private DistributedCache cache;
+	private String key;
+
+	public void run() {
+		cache = new DistributedCache();
+		cache.init();
+
+		while (true) {
+			key = Thread.currentThread().getName() + System.currentTimeMillis();
+			put(key);
+			get(key);
+		}
+	}
+
+	public void put(String key) {
+		String value = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+		try {
+			log.info("putting " + key + "=" + value);
+			cache.put(key, value);
+			log.info("put is done");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void get(String key) {
+		String value = "";
+		try {
+			log.info("getting " + key);
+			value = cache.get(key);
+			if (value == null) {
+				value = "";
+			}
+			log.info("got " + key + "=" + value);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
